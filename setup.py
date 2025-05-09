d1fd6c35966f57ccc9a152faf40b907956657381
nafaraf <cuetnafaraf@gmail.com>
2025-05-09 16:52:54 +0600

    project structure ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='CICD with AWS S3 and Docker',
+    author='Nafiul Araf',
+    license='',
+)
--- src/data_extract.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("jessicali9530/kuc-hackathon-winter-2018")
-
-print("Path to dataset files:", path)
